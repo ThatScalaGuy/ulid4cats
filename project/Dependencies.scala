5d5c31dcfa652bb732330792d6f2050d3a8a5078
Scala Steward <scala_steward@virtuslab.com>
2022-12-20 20:26:37 +0000

    Update airframe-ulid to 22.12.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,6 +2,6 @@ import sbt._
 
 object Dependencies {
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.4.2"
-  lazy val airframeUlid = "org.wvlet.airframe" %% "airframe-ulid" % "22.12.4"
+  lazy val airframeUlid = "org.wvlet.airframe" %% "airframe-ulid" % "22.12.5"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.14"
 }
