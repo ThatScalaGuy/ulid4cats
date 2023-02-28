ac67406b4c8ae148ae0a492e45b282585dc947af
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-02-28 08:49:34 +0100

    Patch updates (#41)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.4.7"
-  lazy val airframeUlid = "org.wvlet.airframe" %% "airframe-ulid" % "23.2.4"
+  lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.4.8"
+  lazy val airframeUlid = "org.wvlet.airframe" %% "airframe-ulid" % "23.2.5"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.15"
 }
