803f9cef853d0de6ee6528db30d4c8b18806e6ed
Scala Steward <scala_steward@virtuslab.com>
2023-04-17 21:53:28 +0000

    Update cats-effect to 3.4.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.4.8"
+  lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.4.9"
   lazy val airframeUlid = "org.wvlet.airframe" %% "airframe-ulid" % "23.3.4"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.15"
 }
