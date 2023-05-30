23e35d56f3261a71eb323c79292677c2227cbc32
Scala Steward <scala_steward@virtuslab.com>
2023-05-30 01:09:39 +0000

    Update scalatest to 3.2.16

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,5 +3,5 @@ import sbt._
 object Dependencies {
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.5.0"
   lazy val airframeUlid = "org.wvlet.airframe" %% "airframe-ulid" % "23.4.6"
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.15"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.16"
 }
