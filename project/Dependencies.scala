b3d366c8c44ce25d8f9e2c616bc24076de9ea9b7
Scala Steward <scala_steward@virtuslab.com>
2023-07-04 22:47:00 +0000

    Revert commit(s) 16ea353, 958d142, 419d246, 1ddabb3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,6 +2,6 @@ import sbt._
 
 object Dependencies {
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.5.0"
-  lazy val airframeUlid = "org.wvlet.airframe" %% "airframe-ulid" % "23.6.2"
+  lazy val airframeUlid = "org.wvlet.airframe" %% "airframe-ulid" % "23.4.8"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.16"
 }
