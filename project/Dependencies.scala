48b7e4d45cc475361d44ed25a71f91d002eb3294
Scala Steward <scala_steward@virtuslab.com>
2023-10-04 03:49:49 +0000

    Update cats-effect to 3.5.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.5.1"
+  lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.5.2"
   lazy val airframeUlid = "org.wvlet.airframe" %% "airframe-ulid" % "23.8.6"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.17"
 }
