feaf1dfb8c0a4e09e1fde7538cf421dfa4ac7bc5
Scala Steward <scala_steward@virtuslab.com>
2025-12-12 23:16:12 +0000

    Update munit-cats-effect to 2.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,5 +6,5 @@ object Dependencies {
   lazy val catsEffect = Def.setting("org.typelevel" %%% "cats-effect" % "3.6.1")
   lazy val munit = Def.setting("org.scalameta" %%% "munit" % "1.0.3")
   lazy val munitCatsEffect =
-    Def.setting("org.typelevel" %%% "munit-cats-effect" % "2.0.0")
+    Def.setting("org.typelevel" %%% "munit-cats-effect" % "2.1.0")
 }
